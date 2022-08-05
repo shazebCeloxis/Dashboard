75b47acc6860c942e27a69daca46a80cab97a965
shazebCeloxis <md.shazeb@celoxis.com>
2022-08-05 19:34:07 +0530

    second commit

--- data.js
+++ data.js
@@ -2,11 +2,27 @@ const http = require('http')
 const fs = require('fs')
 const path = require('path')
 const d = require('./report.js');
+const data = require('./gridData')
+const url = require('url')
 
 http.createServer((req, res) => {
 
     // if (req.params)
-    if (req.url === '/dashboard/report') {
+
+
+    if (req.url === '/dashboard/report/portrail?page=1') {
+        const q = url.parse(req.url, true).query
+        // const myUrl = new URL(req.url)
+        // console.log(myUrl.searchParams)
+        // res.end(q.page)
+
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(data))
+
+    }
+
+
+    else if (req.url === '/dashboard/report') {
 
         {
             res.writeHead(200, 'Content-Head', 'application/json')
