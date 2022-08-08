ff24c8f0bb1149c80b3f7444f880b43901f184e0
shazebCeloxis <md.shazeb@celoxis.com>
2022-08-08 12:41:45 +0530

    data added

--- data.js
+++ data.js
@@ -12,16 +12,40 @@ http.createServer((req, res) => {
 
     if (req.url === '/dashboard/report/portrail?page=1') {
         const q = url.parse(req.url, true).query
+        console.log(q.page)
+        res.end(JSON.stringify(data.gridData))
         // const myUrl = new URL(req.url)
         // console.log(myUrl.searchParams)
         // res.end(q.page)
+        // const VariedData = data.slice(1)
+        // console.log(VariedData)
+        // if (q.page === 1) {
+        //     res.writeHead(200, { 'Content-Type': 'application/json' })
+        //     res.end(JSON.stringify(data.gridData))
 
-        res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify(data))
+        // }
+
+        // else {
+        //     const pageSize = data.gridData.pageSize
+        //     res.writeHead(200, { 'Content-Type': 'application/json' })
+        //     res.end(data.rowData.slice(pageSize * (q.page - 1), (pageSize * q.page) - 1))
+        // }
+
+
+
 
     }
 
+    else if (req.url === '/dashboard/report/portrail?page=2') {
+        const q = url.parse(req.url, true).query
 
+        const pageSize = (data.gridData)[0].pageSize
+        console.log("page", pageSize)
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(data.rowData.slice(pageSize, pageSize * 2 - 1)))
+
+
+    }
     else if (req.url === '/dashboard/report') {
 
         {
